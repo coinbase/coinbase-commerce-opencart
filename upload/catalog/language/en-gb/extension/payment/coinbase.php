9e76c0609ea63ccffea8e0dce509f7315af1f6a4
Igor Meleshko <imeleshko123@gmail.com>
2018-11-06 23:54:35 +0200

    Added pending status and cancel url. (#2)
    * Added pending status and cancel url.
    * Added coinbase commerce link

--- upload/catalog/language/en-gb/extension/payment/coinbase.php
+++ upload/catalog/language/en-gb/extension/payment/coinbase.php
@@ -1,4 +1,4 @@
 <?php
 
-$_['text_title'] = 'Coinbase Commerce';
+$_['text_title'] = 'Coinbase Commerce <a href="https://commerce.coinbase.com/" target="_blank" rel="noopener">(Learn more)</a>';
 $_['button_confirm'] = 'Pay with Coinbase Commerce';
